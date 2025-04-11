878ec1d788392595e390488e16b665dd075807c6
Sashank Baral <sb04041720@student.ku.edu.np>
2025-04-11 12:11:00 +0545

    login.css

--- frontend/client/src/components/login/login.jsx
+++ frontend/client/src/components/login/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from '../../api/axios';
 import { useNavigate } from 'react-router-dom';
-import './Login.css'; // ⬅️ Import the CSS file
+import './login.css'; // ⬅️ Import the CSS file
 
 const Login = () => {
   const navigate = useNavigate();
